e7108342107787642873dd8ff8d400b4b0ec41bb
qin <11888171@qq.com>
2025-12-31 18:42:58 +0800

    Changes to be committed: 	modified:   Html2Md/src/main/java/org/example/Main.java 	modified:   Html2Md/src/main/java/org/example/readability/GetContent.java

--- Html2Md/src/main/java/org/example/Main.java
+++ Html2Md/src/main/java/org/example/Main.java
@@ -4,7 +4,6 @@ import io.reactivex.rxjava3.subjects.BehaviorSubject;
 import org.example.file.FileWriter;
 import org.example.readability.AnalyzeByJSoup;
 import org.example.readability.GetContent;
-import org.example.readability.Readability;
 import org.example.replacerchain.Html2Md;
 import org.jsoup.Jsoup;
 import org.jsoup.safety.Safelist;
@@ -12,7 +11,7 @@ import java.util.Optional;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Html2Md {
+public class Main {
     public static void main(String[] args) {
         String url = "https://blog.csdn.net/u010445301/article/details/111322569?spm=1001.2014.3001.5506";
         String url2 = "https://blog.csdn.net/weixin_45395059/article/details/126006369?spm=1001.2014.3001.5506";
@@ -23,8 +22,8 @@ public class Html2Md {
         observeUrl.map(s -> RetrofitManager.getInstance().getBodyStrSync(s))
                 .filter(Optional::isPresent)
                 .map(Optional::get)
-//                .map(GetContent::getContent)
-                .map(s ->  new AnalyzeByJSoup().parse(s).getString("id.content_views.0@all"))
+                .map(content -> GetContent.getContent(content, observeUrl.getValue()))
+//                .map(s ->  new AnalyzeByJSoup().parse(s).getString("id.content_views.0@all"))
                 .map(html -> UrlResolver.resolveUrlByJsoup(html, observeUrl.getValue()))
                 .map(s -> Jsoup.clean(s, Safelist.relaxed()))
 //                .map(s -> Html2Md.getInstance().parse(observeUrl.getValue(), s))
--- Html2Md/src/main/java/org/example/readability/GetContent.java
+++ Html2Md/src/main/java/org/example/readability/GetContent.java
@@ -1,9 +1,24 @@
 package org.example.readability;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class GetContent {
-    public static String getContent(String html) {
-        Readability readability = new Readability(html);
-        readability.init();
-        return readability.getContent();
+    private static Map<String, String> webPageContentLabels = new HashMap<>();
+    static{webPageContentLabels.put("https://blog.csdn.net/", "id.content_views.0@all");}
+    public static String getContent(String content, String url) {
+        return webPageContentLabels.keySet().stream()
+                .filter(url::startsWith)
+                .findFirst()
+                .map(key -> {
+                    System.out.println("Get content from " + key);
+                    return new AnalyzeByJSoup().parse(content).getString(webPageContentLabels.get(key));
+                })
+                .orElseGet(() -> {
+                    System.out.println("Get content from default");
+                    Readability readability = new Readability(content);
+                    readability.init();
+                    return readability.getContent();
+                });
     }
 }
